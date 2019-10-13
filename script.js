120495d3ab5e3354ede2201d5661dd536052161d
c03rcion <areeves9@icloud.com>
2019-10-13 00:46:51 -0700

    refactored xhr requests as separate function returning a Promise

--- script.js
+++ script.js
@@ -26,6 +26,7 @@ const appState = {
     incidentMarkers: [],
     riskByHourScore: [],
     riskHours: [],
+    totalIncidents: null
 };
 
 const metaData = {
@@ -96,6 +97,45 @@ function setRiskByHourScore(objectArray) {
     })
 };
 
+function createXHRequest(method, url) {
+    // API GET REQUEST TO CRIME INCIDENTS SERVER
+    const xhr = new XMLHttpRequest();
+    return new Promise((resolve, reject) => {
+        // listen for completed request, then process
+        xhr.onreadystatechange = () => {
+            // Only run if the request is complete
+            if (xhr.readyState !== 4) return;
+            // Process the response
+			if (xhr.status >= 200 && xhr.status < 300) {
+				// If successful
+				resolve(xhr);
+			} else {
+				// If failed
+				reject({
+					status: xhr.status,
+					statusText: xhr.statusText
+				});
+            };
+        };
+        // HTTP request
+        xhr.open(method, url);
+        // send HTTP request
+        xhr.send();
+    });
+};
+    // xhr.open(method, url);
+    // xhr.responseType = 'json';
+    // xhr.send();
+    // xhr.onload = function() {
+    //     if (xhr.status != 200) {
+    //         console.log(`Error ${xhr.status}: ${xhr.statusText}`);
+    //     } else {
+    //         const response = xhr.response;
+    //         return response;
+    //     };
+    // };
+
+
 // get prediction and render two chart.js instances - risk score and risk over time
 function getPrediction() {
     // API POST REQUEST TO PREDICTION SERVER
@@ -113,13 +153,7 @@ function getPrediction() {
             const riskByHour = response.riskbyhour.data;
             // update application state
             appState.prediction = response;
-         
-
-
-            setRiskByHourScore(riskByHour);
-
-
-
+            setRiskByHourScore(riskByHour); 
             // create a marker instance with hard-coded lat,lng for test purposes
             const currentLocationMarker = L.marker(
                 [37.759101, -122.414791], {icon: greenIcon}).addTo(mymap);
@@ -146,64 +180,65 @@ function getPrediction() {
     };
 }
 
+function renderMapAdjacentHTML(totalIncidents) {
+    const map = document.getElementById("mapid");
+    const totalMarkup = `
+    <h6 class="text-center">Showing ${appState.incidents.length} of ${totalIncidents} Incidents</h6>
+    <p class='text-center'><button onclick="getNextVehicleIncidents()">More Incidents</button></p>
+    `;
+    map.insertAdjacentHTML('beforebegin', totalMarkup);
+};
+
 
 function getVehicleIncidents() {
     // API GET REQUEST TO CRIME INCIDENTS SERVER
-    let xhr1 = new XMLHttpRequest();
-    xhr1.open('GET', services.vehicleCrimeApi);
-    xhr1.responseType = 'json';
-    xhr1.send();
-    xhr1.onload = function() {
-        if (xhr1.status != 200) {
-            console.log(`Error ${xhr1.status}: ${xhr1.statusText}`);
-        } else {
-            let response = xhr1.response;
-            appState.currentPageVehicleCrimeAPI = response.meta.current_page;
-            const { total_incidents, per_page } = response.meta;
-            const vehicleIncidents = response.vehicle_incidents;
+    createXHRequest('GET', services.vehicleCrimeApi)
+        .then((xhr) => {
+            jsonResponse = JSON.parse(xhr.response);
+            // extract data from JSON
+            const { current_page, total_incidents } = jsonResponse.meta;
+            const vehicleIncidents = jsonResponse.vehicle_incidents;
             // update application state
+            appState.currentPageVehicleCrimeAPI = current_page;
             appState.incidents = vehicleIncidents;
+            appState.totalIncidents = total_incidents;
+            // instantiate leaflet.js object with markers
             mymap.addLayer(createIncidentMarkers(appState.incidents));
+            // renderHTML();
+            renderMapAdjacentHTML(appState.totalIncidents);
+        })
+        .catch((error) => {
+            console.log("Something is wrong", error);
+        });
+};
 
-            const map = document.getElementById("mapid");
-            const totalMarkup = `<h6 class="text-center">Showing ${per_page} of ${total_incidents}</h6>
-            <p class='text-center'><button onclick="hotdogScript()">More Incidents</button></p>`;
-            map.insertAdjacentHTML('beforebegin', totalMarkup);
 
-        };
-    };
-}
-
-function hotdogScript() {
+function getNextVehicleIncidents() {
     // clear previous markers from the map
     appState.incidentMarkers.clearLayers();
-
-    let xhr1 = new XMLHttpRequest();
-    xhr1.open('GET', services.vehicleCrimeApi + `?page=${appState.currentPageVehicleCrimeAPI + 1}`);
-    xhr1.responseType = 'json';
-    xhr1.send();
-    xhr1.onload = function() {
-        if (xhr1.status != 200) {
-            console.log(`Error ${xhr1.status}: ${xhr1.statusText}`);
-        } else {
-            let response = xhr1.response;
-            console.log(response);
-            appState.currentPageVehicleCrimeAPI = response.meta.current_page;
-            // const { total_incidents, per_page } = response.meta;
-            const vehicleIncidents = response.vehicle_incidents;
-            appState.incidents = [...appState.incidents, ...vehicleIncidents];
-            console.log(appState.incidents);
-            // mymap.addLayer(createIncidentMarkers(appState.incidents));
-
-            // const map = document.getElementById("mapid");
-            // const totalMarkup = `<h6 class="text-center">Showing ${per_page} of ${total_incidents}</h6>
-            // <p class='text-center'><button onclick="hotdogScript()">More Incidents</button></p>`;
-            // map.insertAdjacentHTML('beforebegin', totalMarkup);
-
-        };
-    };
+    // request to next page of incident objects
+    createXHRequest('GET', services.vehicleCrimeApi + `?page=${appState.currentPageVehicleCrimeAPI + 1}`)
+        .then((xhr) => {
+            jsonResponse = JSON.parse(xhr.response);
+            // extract data from JSON
+            const { current_page, total_incidents } = jsonResponse.meta;
+            // update application state
+            appState.currentPageVehicleCrimeAPI = current_page;
+            appState.incidents = [...appState.incidents, ...jsonResponse.vehicle_incidents];
+            appState.totalIncidents = total_incidents;
+             // add markers to map instance
+            mymap.addLayer(createIncidentMarkers(appState.incidents));
+            renderMapAdjacentHTML(appState.totalIncidents);
+        })
+        .catch((error) => {
+            console.log("Something is wrong", error);
+        });
 }
 
+// take state obj prop incidents arr of objs
+// and iterate through each obj creating a maker
+// for the map and binding a popup, update 
+// app state with  markerCluseterGroup instance 'markers' 
 function createIncidentMarkers(incidents) {
     // create cluster instance
     const markers = L.markerClusterGroup();
